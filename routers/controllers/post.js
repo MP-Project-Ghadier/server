9319a8634f7b9499103fbb3e904d236fe5a1a892
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-12-23 09:34:14 +0300

    post routers are done

--- routers/controllers/post.js
+++ routers/controllers/post.js
@@ -1,4 +1,5 @@
 const postModel = require("./../../db/models/posts");
+const commentModel = require("./../../db/models/comments");
 
 // new post by user
 const newPost = async (req, res) => {
@@ -101,13 +102,9 @@ const getEvent = (req, res) => {
   }
 };
 
-//there is a problem with this code, 
-//but i feel sleepy so I will continue tomorrow ان شاء الله
 const getPostById = (req, res) => {
   const { id } = req.params;
   try {
-    console.log("result");
-
     postModel.findById(id).then(async (result) => {
       res.status(200).json(result);
     });
@@ -116,6 +113,113 @@ const getPostById = (req, res) => {
   }
 };
 
+const getResearchById = (req, res) => {
+  const { id } = req.params;
+  try {
+    postModel.findById(id).then(async (result) => {
+      res.status(200).json(result);
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const getEventById = (req, res) => {
+  const { id } = req.params;
+  try {
+    postModel.findById(id).then(async (result) => {
+      res.status(200).json(result);
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+//delete a post
+const deletePost = (req, res) => {
+  const { id } = req.params;
+  try {
+    postModel.findOne({ post: id, isDel: false }).then((result) => {
+      if (result) {
+        postModel
+          .findByIdAndUpdate(id, { isDel: true }, { new: true })
+          .exec()
+          .then((result) => {
+            res.status(200).json(result);
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      }
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+//delete any post by admin
+const deletePostByAdmin = (req, res) => {
+  const { id } = req.params;
+  try {
+    postModel
+      .findByIdAndUpdate(id, { isDel: true }, { new: true })
+      .exec()
+      .then((result) => {
+        res.status(200).json(result);
+      });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+// all comments of the post
+const postComments = async (req, res) => {
+  const { id } = req.params; //post id
+  try {
+    let allPost = [];
+    postModel
+      .findOne({ _id: id }, { isDel: false })
+      .populate("user")
+      .then((result) => {
+        allPost.push(result);
+        commentModel
+          .find({ post: id })
+          .populate("user", "name")
+          .then((result2) => {
+            allPost.push(result2);
+            res.status(200).json(allPost);
+          });
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const updatePost = (req, res) => {
+  const { id } = req.params;
+  const { title, desc } = req.body;
+  try {
+    postModel
+      .findOne({ post: id, user: req.token.id, isDel: false })
+      .then((result) => {
+        if (result) {
+          postModel
+            .findByIdAndUpdate(id, { title, desc, _id: id }, { new: true })
+            .then((result) => {
+              res.status(200).json(result);
+            });
+        }
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 module.exports = {
   newPost,
   newResearch,
@@ -124,4 +228,10 @@ module.exports = {
   getResearch,
   getEvent,
   getPostById,
+  getResearchById,
+  getEventById,
+  deletePost,
+  deletePostByAdmin,
+  postComments,
+  updatePost,
 };
--- routers/routes/post.js
+++ routers/routes/post.js
@@ -10,16 +10,33 @@ const {
   newEvent,
   getPosts,
   getResearch,
-  getEvent, 
-  getPostById
+  getEvent,
+  getPostById,
+  getResearchById,
+  getEventById,
+  deletePost,
+  deletePostByAdmin,
+  postComments,
+  updatePost,
 } = require("../controllers/post");
 
 postRouter.post("/newPost", authentication, newPost); //by any user
 postRouter.post("/newResearch", authentication, authorization, newResearch); //only by admin or specialist
 postRouter.post("/newEvent", authentication, authorization, newEvent); //only by admin or specialist
-postRouter.get("/getPosts", getPosts);
+postRouter.get("/getPosts", authentication, getPosts);
 postRouter.get("/getResearch", authentication, getResearch);
 postRouter.get("/getEvent", authentication, getEvent);
-postRouter.get("/getPostById", getPostById);
+postRouter.get("/getPostById/:id", authentication, getPostById);
+postRouter.get("/getResearchById/:id", authentication, getResearchById);
+postRouter.get("/getEventById/:id", authentication, getEventById);
+postRouter.put("/deletePost/:id", authentication, deletePost);
+postRouter.put(
+  "/deletePostByAdmin/:id",
+  authentication,
+  authorization,
+  deletePostByAdmin
+);  //only by admin or specialist
+postRouter.get("/postComments/:id", authentication, postComments);
+postRouter.put("/updatePost/:id", authentication, updatePost);
 
 module.exports = postRouter;
